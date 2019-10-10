bc0648bf5373f5ec6c64521e07d3ad9159a97e95
Ingvald <ingvald.orbeck@gmail.com>
2019-10-10 16:48:13 +0200

    Updates

---
+++ precache-manifest.9d6b8264f3a9075b586f7e0ef2482dea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0207692ffeadc86fcebc4802f5997768",
+    "url": "/react-training/index.html"
+  },
+  {
+    "revision": "8cda1ea6ac2d3bcf2432",
+    "url": "/react-training/static/css/main.1bf2786b.chunk.css"
+  },
+  {
+    "revision": "e56ed1d70b373a25b7e5",
+    "url": "/react-training/static/js/2.4ba27be9.chunk.js"
+  },
+  {
+    "revision": "8cda1ea6ac2d3bcf2432",
+    "url": "/react-training/static/js/main.69437873.chunk.js"
+  },
+  {
+    "revision": "8645fb1d699588baff8a",
+    "url": "/react-training/static/js/runtime-main.2c5469c0.js"
+  }
+]);
